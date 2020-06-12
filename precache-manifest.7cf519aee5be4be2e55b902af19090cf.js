7db626bdd975073a074e4154b84ea0b6ef44fa45
Jashann <48613250+Jashann@users.noreply.github.com>
2020-06-12 10:14:30 +0530

    Updates

---
+++ precache-manifest.7cf519aee5be4be2e55b902af19090cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b53b7d6e0c26018d25ae1154eb03ce",
+    "url": "/react_avator/index.html"
+  },
+  {
+    "revision": "8a470eb03714f527d553",
+    "url": "/react_avator/static/css/main.978d86d7.chunk.css"
+  },
+  {
+    "revision": "00fcca3ec42227361113",
+    "url": "/react_avator/static/js/2.465df07d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_avator/static/js/2.465df07d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a470eb03714f527d553",
+    "url": "/react_avator/static/js/main.25a8d542.chunk.js"
+  },
+  {
+    "revision": "3c43273fbc10a1f492d6",
+    "url": "/react_avator/static/js/runtime-main.17218d04.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_avator/static/media/logo.5d5d9eef.svg"
+  }
+]);
